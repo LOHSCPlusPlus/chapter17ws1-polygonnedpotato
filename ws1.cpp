c31e0568a19eebcaae9ac5db723ee9435f7807c2
elburg <>
2022-05-06 21:49:57 +0000

    done with ws1

--- ws1.cpp
+++ ws1.cpp
@@ -1,42 +1,105 @@
 #include <iostream>
+#define n Node
+#define SENTINEL -999 
+#define p nullptr
 using namespace std;
 
 // Part 1.1 Complete the node declaration
-struct Node
+struct n
 {
+n(int);
+int data;
+n *next;
 };
-
+n::n(int Data){
+data=
+  Data;
+next=
+  p;
+}
 // Part 1.2 Define the Node constructor
 
 //constants
-const int SENTINEL = -999;
+// const int SENTINEL = -999;
 
 //print function
-void printList(Node *head);
-
+void printList(n *head);
+  
 //main
 int main()
 {
-    Node *head = nullptr;
-    Node *tail = nullptr;
-    int num = 0;
-    cout << "Enter a number to add to the list (" << SENTINEL << " to end)";
-    cin >> num;
-    while (num != SENTINEL) {
+    n *head = 
+  p;
+    n *tail = 
+  p;
+    int num = 
+  0;
+    cout << 
+      "Enter a number to add to the list (" 
+      << 
+      SENTINEL 
+      << 
+      " to end)"
+      ;
+    cin 
+      >> 
+        num
+          ;
+    while 
+      (num 
+        != 
+          SENTINEL
+      ) {
         // Part 2, create a new node pointer, and append it to the list.
-    
-        cout << "Enter a number to add to the list (" << SENTINEL << " to end)";
-        cin >> num;
+      n *ptr=new n(num);  
+      if(
+        tail==p
+      ){
+        head=/*new n(num)*/ptr;
+      }else{
+        tail->next=/*new n(num)*/ptr;
+      };
+      tail=/*new n(num)*/ptr;
+      cout << "Enter a number to add to the list (" << SENTINEL << " to end)";
+      cin >> num;
     }
 
 	printList(head);
     
     // Part 3, delete the list
-
-
+    while(head!=p){n *next=head->next;delete head;head=next;}
+    tail=p;
+    delete tail;
     return 0;
 }
 
 // Part 4 fill in the print function to print the list
-void printList(Node *head) {
+void printList(n *head) {
+  n 
+  *c
+  =
+  head
+  ;
+  while
+  (
+  c
+  !=
+  p
+  )
+  {
+  cout
+  <<
+  c
+  ->
+  data
+  <<
+  endl
+  ;
+  c
+  =
+  c
+  ->
+  next
+  ;
+  }
 }
